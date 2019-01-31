d11b21530bb77e671faa5b4ddff441378acce54f
valdemarian <xxxxxlxxx@mail.ru>
2019-01-31 22:14:23 +0300

    Updates

---
+++ precache-manifest.c96a276842eecd69b30b3fd89e3685a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "281fcec118589f25a466",
+    "url": "/stardb/static/css/main.52c78603.chunk.css"
+  },
+  {
+    "revision": "281fcec118589f25a466",
+    "url": "/stardb/static/js/main.281fcec1.chunk.js"
+  },
+  {
+    "revision": "7733754c3a72e838c46c",
+    "url": "/stardb/static/js/1.7733754c.chunk.js"
+  },
+  {
+    "revision": "e2dba1e2e722e3a72ca8",
+    "url": "/stardb/static/js/runtime~main.e2dba1e2.js"
+  },
+  {
+    "revision": "b5ab6393fbd212779cb5f754c7264125",
+    "url": "/stardb/index.html"
+  }
+];
